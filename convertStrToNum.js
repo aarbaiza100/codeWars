5bcd47d86285ab93990a3ce9cc612d4167b35aa4
aarbaiza100 <38160729+aarbaiza100@users.noreply.github.com>
2022-03-17 12:43:54 -0500

    katas
    added two new katas

--- convertStrToNum.js
+++ convertStrToNum.js
@@ -1,3 +1,5 @@
+//simple function that can transform a string into a number 
+
 var stringToNumber = function(str){
    return parseInt(str);
 
---
+++ numToString.js
@@ -0,0 +1,6 @@
+// write a simple function to return a number to a string 
+
+function numberToString(num) {
+    return num.ToString();
+  }
+  
