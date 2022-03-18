ad46c42ea52648a8cf86703a2caa060161c3107c
aarbaiza100 <38160729+aarbaiza100@users.noreply.github.com>
2022-03-18 13:04:51 -0500

    more problems

---
+++ keepHydrated.js
@@ -0,0 +1,13 @@
+//My OG solution
+// function litres(time) {
+//     let product = 0.5 * time;
+//     while(time > 0) {
+//       return Math.floor(product);
+//     }
+//       return Math.floor(product);
+//     }
+
+    
+    function litres(time) {
+        return Math.floor(time * 0.5);
+      }
