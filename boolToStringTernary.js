804389f5a0228abff4711c3b01f6b6083a665ba5
aarbaiza100 <38160729+aarbaiza100@users.noreply.github.com>
2022-03-22 10:10:30 -0500

    daily push

---
+++ boolToStringTernary.js
@@ -0,0 +1,8 @@
+function boolToWord( bool ){
+    return (bool ? "Yes" : "No");
+  }
+  // we must return a value using b.toString();
+  // if value == true then the function must return "yes";
+  //if value == false, then function must return "no";
+  
+  
