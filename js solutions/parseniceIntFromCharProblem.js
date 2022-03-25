908a0fc45071c9a31a4d983f51135e1db43956a1
aarbaiza100 <38160729+aarbaiza100@users.noreply.github.com>
2022-03-25 15:02:09 -0500

    daily push

---
+++ js solutions/parseniceIntFromCharProblem.js
@@ -0,0 +1,9 @@
+//for correct answer program should return int from 0 to 9
+//assume text input string always valid and may look like "1 year old" or "5 years old"
+//The first char is number only 
+
+function getAge(inputString){
+    return Number(inputString[0]);
+    }
+    
+    
