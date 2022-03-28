f0c109395fc2f101eaf69da45d02a676a6cc96de
aarbaiza100 <38160729+aarbaiza100@users.noreply.github.com>
2022-03-28 14:11:51 -0500

    daily push

---
+++ js solutions/stringToArray.js
@@ -0,0 +1,20 @@
+
+  //write a function to split a string and convert it into an array of words
+  //stringToarray = function (str) {
+
+  //}
+
+  //convert it into an array of words 
+  // return str.trim().split("");
+
+  //trim()
+  //removes white space from both ends of a string and returns a new string
+
+  //split()
+  // divides string into an ordered list of substrings
+  // puts these substrings into an array
+  // returns the array 
+
+  stringToArray = function (str) {
+    return str.trim().split("");
+  }
