80802223f1606cee82a2094f5d3b505e7759d8e3
aarbaiza100 <38160729+aarbaiza100@users.noreply.github.com>
2022-05-11 12:14:33 -0500

    daily push

--- js solutions/totalAmtOfPoints.js
+++ js solutions/totalAmtOfPoints.js
@@ -1,3 +1,19 @@
-const points = games => games.reduce((output,current) => {
-return output += current[0] > current[2] ? 3 : current[0] === current[2] ? 1 : 0; 
-},0);
+// const points = games => games.reduce((output,current) => {
+// return output += current[0] > current[2] ? 3 : current[0] === current[2] ? 1 : 0; 
+// },0);
+
+function points(games){
+    let result = [];
+    let score = 0;
+    games.forEach(x => {
+        result = x.split(":");
+if(parseInt(result[0])>parseInt(result[1]))
+        score += 3;
+        else if(result[0] == result[1])
+        score += 1;
+        else
+            score += 0;
+
+    })
+    return score;
+}
