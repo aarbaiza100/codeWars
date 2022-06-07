618262f9ca5adac54e239f442639fa615cf6b4da
aarbaiza100 <38160729+aarbaiza100@users.noreply.github.com>
2022-06-07 11:59:13 -0500

    daily push

---
+++ howGoodAreYouReally.js
@@ -0,0 +1,5 @@
+function betterThanAverage(classPoints, yourPoints) {
+    let average = classPoints.reduce((a,b) => a + b, 0) / classPoints.length;
+
+    return yourPoints > average ? true : false; 
+}
