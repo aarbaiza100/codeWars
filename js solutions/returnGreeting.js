809dee433ed1ac98681464ca4337d9704411c444
Alexis Arbaiza <38160729+aarbaiza100@users.noreply.github.com>
2022-06-18 19:31:37 -0500

    Update returnGreeting.js

--- js solutions/returnGreeting.js
+++ js solutions/returnGreeting.js
@@ -2,5 +2,4 @@ function greet(name){
    return `Hello, ${name} how are you doing today?`;
   }
 
-  //return a greeting statement that uses an input 
-  //program shoulder return EXACTLY "Hello, <name> how are you doing today?"
+  
