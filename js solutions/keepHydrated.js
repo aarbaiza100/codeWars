5493401a5102dc4dfb707d99d5d85585396b8583
Alexis Arbaiza <38160729+aarbaiza100@users.noreply.github.com>
2022-07-20 15:52:19 -0500

    Update keepHydrated.js

--- js solutions/keepHydrated.js
+++ js solutions/keepHydrated.js
@@ -1,11 +1,4 @@
-//My OG solution
-// function litres(time) {
-//     let product = 0.5 * time;
-//     while(time > 0) {
-//       return Math.floor(product);
-//     }
-//       return Math.floor(product);
-//     }
+
 
     
     function litres(time) {
